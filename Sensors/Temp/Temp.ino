20d743c626511993510331e160f8cd5721b93ef4
Victor Alonso Altamirano Izquierdo <v.a.i@hotmail.com>
2021-01-30 00:44:57 -0600

    update

--- Sensors/Temp/Temp.ino
+++ Sensors/Temp/Temp.ino
@@ -1,10 +1,9 @@
 #include <WiFi.h>
 #include "Esp32MQTTClient.h"
+#include <Wire.h>
+#include <Adafruit_MLX90614.h>
 
-#define packageSize 250
-#define ESP_getChipId()   ((uint32_t)ESP.getEfuseMac())
-
-int hrArray[packageSize * 2];
+float tempAVG=0;
 int counter = 0;
 
 // Please input the SSID and password of WiFi
@@ -17,6 +16,8 @@ static const char* connectionString = "xxxxxxxxx";
 
 static bool hasIoTHub = false;
 
+bool flag = false;
+
 char* string2char(String command) {
   if (command.length() != 0) {
     char *p = const_cast<char*>(command.c_str());
@@ -24,27 +25,36 @@ char* string2char(String command) {
   }
 }
 
-volatile int interruptCounter;
-int totalInterruptCounter;
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
-hw_timer_t * timer = NULL;
-portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
-
-void IRAM_ATTR onTimer() {
-  portENTER_CRITICAL_ISR(&timerMux);
-  int sensorValue = analogRead(A2);
-  hrArray[counter] = sensorValue;
-  counter++;
-  portEXIT_CRITICAL_ISR(&timerMux);
+float correlation(float amb, float skin) {
+  if (skin > 27 && skin < 36) {
+    float realTemp = 0.71429 * skin - 0.35714 * amb + 23.14286;
+    return realTemp;
+  }
+  return skin;
 }
 
-void checkWifi(){
+void temp(){
+  float temp = correlation(mlx.readAmbientTempC(), mlx.readObjectTempC());
+  if(temp>35 && counter<5){
+  tempAVG+=temp;
+  counter++;
+  }
+  if(counter==5){
+    flag=true;
+  }
+  Serial.println(temp);
+  delay(500);
+}
+
+void checkWifi() {
   int conCounter = 0;
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.print(".");
     conCounter++;
-    if(conCounter==10){
+    if (conCounter == 10) {
       ESP.restart();
     }
   }
@@ -52,6 +62,7 @@ void checkWifi(){
 
 void setup() {
   Serial.begin(115200);
+  mlx.begin();
   Serial.println("Starting connecting WiFi.");
   delay(10);
   WiFi.begin(ssid, password);
@@ -65,29 +76,23 @@ void setup() {
     return;
   }
   hasIoTHub = true;
-  timer = timerBegin(0, 80, true);
-  timerAttachInterrupt(timer, &onTimer, true);
-  timerAlarmWrite(timer, 4000, true);
-  timerAlarmEnable(timer);
 }
 
 void loop() {
-  checkWifi();
-  if (counter > packageSize) {
+  //checkWifi();
+  temp();
+  if (flag)
+  {
+    Serial.println("Data");
     if (hasIoTHub)
     {
-      String jsonSend = "";
-      for (int i = 0; i < counter; i++) {
-        jsonSend += String(hrArray[i]);
-        if (i != (counter - 1)) {
-          jsonSend += ",";
-        }
-      }
-      counter = 0;
-      char buff[2048];
-      snprintf(buff, 2048, string2char("{\"data\":[" + jsonSend + "],\"pat\":\"1\"}"));
+      String jsonSend = String(tempAVG/5);
+      char buff[128];
+      snprintf(buff, 128, string2char("{\"data\":[" + jsonSend + "],\"pat\":\"1\"}"));
       Esp32MQTTClient_SendEvent(buff);
-      
+      flag = false;
+      tempAVG=0;
+      counter = 0;
     }
   }
 }
